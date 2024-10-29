9b2b7b30896a3786dad349450e73066aa8bf574a
wissem ksentini <wissem.ksentini@gmail.com>
2024-10-29 12:08:37 +0100

    ignore warning of rn-voice lib

--- Layout.tsx
+++ Layout.tsx
@@ -2,6 +2,11 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import RecordingScreen from "./src/screens/RecordingScreen/RecordingScreen";
+import { LogBox } from "react-native";
+
+// remove a common warning coming from react-native-voice.
+// https://github.com/react-native-voice/voice/issues/357#issuecomment-1229499442
+LogBox.ignoreLogs(["new NativeEventEmitter"]);
 
 const Layout: React.FC = () => {
   return (
